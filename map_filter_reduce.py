57e8c1b96089b2e0ab240bc74bd94ab9935fba35
arush <arush.joshi@effem.com>
2022-01-25 11:28:12 +0530

    reduce

--- map_filter_reduce.py
+++ map_filter_reduce.py
@@ -1,3 +1,6 @@
+from importlib_metadata import functools
+
+
 l = [x for x in range(10) if x %2 ==0]
 print(l)
 
@@ -7,3 +10,6 @@ print("mlist",mlist)
 
 o = list(map(lambda x : x**2,mlist))
 print(o)
+import functools
+temp = functools.reduce(lambda x,y:x+y,mlist)
+print(temp)
