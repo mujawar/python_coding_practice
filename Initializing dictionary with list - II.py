33cc65426c15f4e743757797214bcb6a91f47e10
arush <arush.joshi@effem.com>
2022-01-25 10:47:44 +0530

    word count

--- Initializing dictionary with list - II.py
+++ Initializing dictionary with list - II.py
@@ -5,6 +5,7 @@ L = [1,2,4,8,16,32,64,128,256,512,1024,32768,65536,4294967296]
 d = defaultdict(list)
 print(d)
 for i in L:
+    print("d[len(str(i))]",d[len(str(i))],i)
     d[len(str(i))].append(i)
 
 print(d)
